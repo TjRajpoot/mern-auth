34cc7a9311961f350c866d3f291df7d320f18b6e
TjRajpoot <tanuj9340685860@gmail.com>
2025-08-12 10:50:58 +0530

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,5 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'/',
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://mern-authenticator-back.onrender.com',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
