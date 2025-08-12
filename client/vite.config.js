597192c39e0279c99be80633b3250f628b86ef46
TjRajpoot <tanuj9340685860@gmail.com>
2025-08-12 10:49:11 +0530

    Final Commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://mern-authenticator-back.onrender.com',
         changeOrigin: true,
         secure: false
       }
