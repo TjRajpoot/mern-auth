a8146a4f6b8c71591ca6e0e9589a37b8a14c80ab
Tanuj Singh Rajpoot <tanuj9340685860@gmail.com>
2025-08-12 13:30:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,11 +10,11 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['https://mern-auth-frontend-9xyz.onrender.com']
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({ origin:allowedOrigins ,credentials:true }))
+app.use(cors({ origin:allowedOrigins ,methods:["POST","GET"],credentials:true }))
 
 
 //API Endpoints
